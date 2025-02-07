caa67c16b3e5e8baf4f34f33dd96770a436c4733
mrbana69 <bana.emi2007@gmail.com>
2025-02-07 15:27:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previene il comportamento predefinito del form
+
+    const password = document.getElementById('password').value;
+    const correctPassword = 'passwordCorretta'; // Sostituisci con la password corretta
+
+    if (password === correctPassword) {
+        window.location.href = 'seconda-parte_home/home.html'; // Sostituisci con la pagina successiva
+    } else {
+        document.getElementById('errorMessage').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('errorMessage').style.display = 'none';
+            location.reload(); // Ricarica la pagina dopo 3 secondi
+        }, 3000); // Nasconde il messaggio di errore dopo 3 secondi
+    }
+});
