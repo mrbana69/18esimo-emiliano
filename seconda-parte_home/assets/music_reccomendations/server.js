1a6b362344147224d1738d12435fa9175dcb46ba
mrbana69 <bana.emi2007@gmail.com>
2025-02-10 08:38:41 +0100

    Update server.js

--- seconda-parte_home/assets/music_reccomendations/server.js
+++ seconda-parte_home/assets/music_reccomendations/server.js
@@ -17,24 +17,6 @@ apiKey.apiKey = process.env.BREVO_API_KEY;
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-app.post('/send-dedica', (req, res) => {
-    const { dedica } = req.body;
-
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.subject = 'Nuova Dedica';
-    sendSmtpEmail.htmlContent = `<html><body><p>${dedica}</p></body></html>`;
-    sendSmtpEmail.sender = { name: 'Dediche', email: process.env.SENDER_EMAIL };
-    sendSmtpEmail.to = [{ email: process.env.ADMIN_EMAIL }];
-
-    apiInstance.sendTransacEmail(sendSmtpEmail).then(data => {
-        console.log('Email inviata:', data);
-        res.status(200).json({ success: true });
-    }).catch(error => {
-        console.error('Errore nell\'invio dell\'email:', error);
-        res.status(500).json({ success: false });
-    });
-});
-
 // Configurazione nodemailer per raccomandazioni musicali
 app.post('/submit-recommendation', (req, res) => {
     const { songTitle, artistName, songUrl } = req.body;
