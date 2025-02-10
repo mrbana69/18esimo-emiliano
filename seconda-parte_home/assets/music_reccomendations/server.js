0c456729468f4ccb83122633c3409676a274edb1
mrbana69 <bana.emi2007@gmail.com>
2025-02-10 09:05:58 +0100

    Update server.js

--- seconda-parte_home/assets/music_reccomendations/server.js
+++ seconda-parte_home/assets/music_reccomendations/server.js
@@ -4,26 +4,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const SibApiV3Sdk = require('sib-api-v3-sdk');
-const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Configurazione Brevo per dediche
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = process.env.BREVO_API_KEY;
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-app.post('/send-dedica', (req, res) => {
+app.post('/send-reccomendation', (req, res) => {
     const { dedica } = req.body;
 
     const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.subject = 'Nuova Dedica';
-    sendSmtpEmail.htmlContent = `<html><body><p>${dedica}</p></body></html>`;
-    sendSmtpEmail.sender = { name: 'Dediche', email: process.env.SENDER_EMAIL };
+    sendSmtpEmail.subject = 'Nuova Canzone';
+    sendSmtpEmail.htmlContent = `<html><body><h1>${dedica}</h1></body></html>`;
+    sendSmtpEmail.sender = { name: 'Clicca qui', email: process.env.SENDER_EMAIL };
     sendSmtpEmail.to = [{ email: process.env.ADMIN_EMAIL }];
 
     apiInstance.sendTransacEmail(sendSmtpEmail).then(data => {
@@ -32,33 +30,6 @@ app.post('/send-dedica', (req, res) => {
     }).catch(error => {
         console.error('Errore nell\'invio dell\'email:', error);
         res.status(500).json({ success: false });
-    });
-});
-
-// Configurazione nodemailer per raccomandazioni musicali
-app.post('/submit-recommendation', (req, res) => {
-    const { songTitle, artistName, songUrl } = req.body;
-
-    const transporter = nodemailer.createTransport({
-        service: 'SendinBlue', // Brevo è precedentemente conosciuto come SendinBlue
-        auth: {
-            user: process.env.BREVO_EMAIL,
-            pass: process.env.BREVO_API_KEY
-        }
-    });
-
-    const mailOptions = {
-        from: process.env.BREVO_EMAIL,
-        to: process.env.ADMIN_EMAIL,
-        subject: 'New Song Recommendation',
-        text: `Song Title: ${songTitle}\nArtist Name: ${artistName}\nSong URL: ${songUrl}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).json({ error: 'Failed to send email' });
-        }
-        res.status(200).json({ message: 'Recommendation submitted successfully' });
     });
 });
 
